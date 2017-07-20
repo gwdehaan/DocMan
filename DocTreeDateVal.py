cb052492a9671e536cbbbe78139c59e734cc2482
Geert De Haan <geertwdehaan@hotmail.nl>
2017-07-20 18:22:09 +0200

    snippet om een dqtum te controleren

---
+++ DocTreeDateVal.py
@@ -0,0 +1,11 @@
+import time
+def is_date_valid(year, month, day):
+    this_date = '%d/%d/%d' % (month, day, year)
+    try:
+        time.strptime(this_date, '%m/%d/%Y')
+    except ValueError:
+        return False
+    else:
+        return True
+        
+print(is_date_valid(2017,11,31))
