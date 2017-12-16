ae839bf0965fa83c84e4cc42eaf65f448259c730
gwdehaan <geertwdehaan@hotmail.nl>
2017-12-16 18:40:19 +0100

    Herstel

--- TryViewAppArch1.py
+++ TryViewAppArch1.py
@@ -1,4 +1,4 @@
-''''POC DocView GdH 19-11-2017
+'''POC DocView GdH 19-11-2017
 
 POC architectuur Docview 
 doel App op basis van tk.Tk() met twee frames :
@@ -10,9 +10,9 @@ POC
 - TopFrame met label
 - BottomFrame met label
 - App.init()
-    frame
-''''
+    frame'''
 
+'''
 import tkinter as tk
 
 class Root(tk.Tk):
@@ -26,20 +26,33 @@ class Root(tk.Tk):
 if __name__ == "__main__":
     root = Root()
     root.mainloop()
-    
+'''   
 
 import tkinter as tk
 
 class Root(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.TF = TopFrame()
+        self.TF.pack()
+        self.BF = BottomFrame()
+        self.BF.pack()
+        # self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
 
-        self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
+        # self.label.pack()
 
-        self.label.pack()
+class TopFrame(tk.Frame):
+    def __init__(self):
+        super().__init__()
+        self.TopLabel = tk.Label(self, text = "I 've been Framed")
+        self.TopLabel.pack()
 
-class TopFrame(frame):
-    def 
+class BottomFrame(tk.Frame):
+    def __init__(self):
+        super().__init__()
+        self.BottomLabel = tk.Label(self, text = "I 've been Framed too")
+        self.BottomLabel.pack()
+
 if __name__ == "__main__":
     root = Root()
     root.mainloop()
